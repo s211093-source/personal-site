61f7d4ae53ac03e2bf3153e5d6f2fad2709eae8e
s211093-source <s211093@stu.tcssh.tc.edu.tw>
2026-04-29 17:40:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
+if (slides.length > 1) {
+  setInterval(() => {
+    slides[currentSlide].classList.remove("active");
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add("active");
+  }, 7000);
+}
